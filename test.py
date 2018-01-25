12be4a1e2be2343e5239273a69555810dd3ac980
Pramod <pramod.kp72@gmail.com>
2018-01-25 10:39:15 -0700

    test.py has been modified

--- test.py
+++ test.py
@@ -12,34 +12,34 @@ app = Flask(__name__)
 @app.route("/homepage")
 def homepage():
         return render_template("homepage.html")
-    # elif request.method == 'POST':
-    #     projectpath = request.form['projectFilepath']
-    #     sparql = SPARQLWrapper('http://localhost:3030/Combined/sparql')
-    #     if projectpath is None or projectpath == " ":
-    #         sparql.setQuery(""" PREFIX auts: <http://www.vehicularguide.com/ontologies/automobiles.owl#>
-    #
-    #                     SELECT ?node ?y WHERE{
-    #
-    #                       ?node auts:hasModel ?y.
-    #
-    #                     } LIMIT 5""")
-    #     else:
-    #         sparql.setQuery(""" PREFIX auts: <http://www.vehicularguide.com/ontologies/automobiles.owl#>
-    #
-    #                     SELECT ?node ?y WHERE{
-    #
-    #                       """ + projectpath + """ auts:hasModel ?y.
-    #
-    #                     } LIMIT 5""")
-    #
-    #     l = " "
-    #     sparql.setReturnFormat(JSON)
-    #     results = sparql.query().convert()
-    #     # for result in results["results"]["bindings"]:
-    #     #     l += (result["y"]["value"] + "\n")
-    #     projectFilepath  = l
-    #     detail = results
-    #     return render_template('homepage.html', details=detail)
+    elif request.method == 'POST':
+        projectpath = request.form['projectFilepath']
+        sparql = SPARQLWrapper('http://localhost:3030/Combined/sparql')
+        if projectpath is None or projectpath == " ":
+            sparql.setQuery(""" PREFIX auts: <http://www.vehicularguide.com/ontologies/automobiles.owl#>
+    
+                        SELECT ?node ?y WHERE{
+    
+                          ?node auts:hasModel ?y.
+    
+                        } LIMIT 5""")
+        else:
+            sparql.setQuery(""" PREFIX auts: <http://www.vehicularguide.com/ontologies/automobiles.owl#>
+    
+                        SELECT ?node ?y WHERE{
+    
+                          """ + projectpath + """ auts:hasModel ?y.
+    
+                        } LIMIT 5""")
+    
+        l = " "
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        # for result in results["results"]["bindings"]:
+        #     l += (result["y"]["value"] + "\n")
+        projectFilepath  = l
+        detail = results
+        return render_template('homepage.html', details=detail)
 
 
 
